ff489ed7d77569dbd0c09cf34f41bc0364d35037
xb <112629549+2403740786@users.noreply.github.com>
2023-04-10 12:50:53 +0800

    Add files via upload

--- omni_PD2154.mk
+++ omni_PD2154.mk
@@ -12,6 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from PD2154 device
 $(call inherit-product, device/vivo/PD2154/device.mk)
 
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
 PRODUCT_DEVICE := PD2154
 PRODUCT_NAME := omni_PD2154
 PRODUCT_BRAND := vivo
