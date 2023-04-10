ab02783cc83d4d8613b596ad2e092c03090dae70
xb <112629549+2403740786@users.noreply.github.com>
2023-04-10 17:23:47 +0800

    Add files via upload

--- twrp_PD2154.mk
+++ twrp_PD2154.mk
@@ -7,10 +7,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/vivo_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from PD2154 device
 $(call inherit-product, device/vivo/PD2154/device.mk)
