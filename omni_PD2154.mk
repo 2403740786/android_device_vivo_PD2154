15a6e2f136501a3b0866552065d98628e1bb3d9b
xb <112629549+2403740786@users.noreply.github.com>
2023-05-10 12:36:13 +0800

    Add files via upload

---
+++ omni_PD2154.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from PD2154 device
+$(call inherit-product, device/vivo/PD2154/device.mk)
+
+PRODUCT_DEVICE := PD2154
+PRODUCT_NAME := omni_PD2154
+PRODUCT_BRAND := vivo
+PRODUCT_MODEL := V2154A
+PRODUCT_MANUFACTURER := vivo
+
+PRODUCT_GMS_CLIENTID_BASE := android-vivo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="qssi-user 13 TP1A.220624.014 eng.compil.20230106.161435 release-keys"
+
+BUILD_FINGERPRINT := vivo/PD2154/PD2154:13/TP1A.220624.014/compiler01061614:user/release-keys
